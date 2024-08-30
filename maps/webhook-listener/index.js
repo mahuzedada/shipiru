1a1f8ca4b5cbf34a397414cc6a129029fc795da6
Adewale Santos <46720223+mahuzedada@users.noreply.github.com>
2024-08-30 09:23:02 -0400

    Implemented simple Amsible setup (#1)
    Implemented a simple Ansible setup

---
+++ maps/webhook-listener/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+  const payload = req.body;
+
+  // Extract repository and branch information
+  const repoUrl = payload.repository.ssh_url;
+  const branch = payload.ref.split('/').pop(); // Extracts the branch name
+  const repoName = payload.repository.name;
+
+  console.log(`Received push on branch ${branch} for repository ${repoName}`);
+
+  const scriptPath = path.resolve(process.env.HOME, 'pipeline', 'build.sh');
+  console.log(`Will execute build script: ${scriptPath} ${repoUrl} ${branch}`);
+
+  // Run the build.sh script with the full path
+  exec(`${scriptPath} ${repoUrl} ${branch}`, (err, stdout, stderr) => {
+
+    if (err) {
+      console.error(`Error executing script: ${err}`);
+    }
+    console.log(`Script output: ${stdout}`);
+    console.error(`Script error output: ${stderr}`);
+  });
+  res.status(200).send('Build triggered successfully');
+});
+
+app.listen(PORT, () => {
+  console.log(`Webhook listener running on port ${PORT}`);
+});
